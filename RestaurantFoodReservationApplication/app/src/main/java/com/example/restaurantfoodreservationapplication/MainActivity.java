dc83ed05ceb9c27504484d983aab6441bf5a8260
NguyenTien243 <nguyenquoctien243@gmail.com>
2021-06-16 23:37:08 +0700

    Main activity

---
+++ RestaurantFoodReservationApplication/app/src/main/java/com/example/restaurantfoodreservationapplication/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.restaurantfoodreservationapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnQLNV,btnQLDatMon,btnQLMonAn,btnQLBan;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnQLNV = (Button) findViewById(R.id.btnQLNhanVien);
+        btnQLDatMon = (Button) findViewById(R.id.btnQLDatmon);
+        btnQLMonAn = (Button) findViewById(R.id.btnQLMonAn);
+        btnQLBan = (Button) findViewById(R.id.btnQLBan);
+
+        btnQLBan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,QLBanAnActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnQLDatMon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,DatMonActivity.class);
+                startActivity(intent);
+            }
+        });
+//        btnQLMonAn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this,.class);
+//                startActivity(intent);
+//            }
+//        });
+        btnQLNV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,QLNhanVienActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
