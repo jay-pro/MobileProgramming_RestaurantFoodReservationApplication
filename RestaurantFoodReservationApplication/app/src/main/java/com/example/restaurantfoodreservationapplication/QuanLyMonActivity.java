4d9f23035398a595c2da548d17a7579687983077
河儿 <60065901+jay-pro@users.noreply.github.com>
2021-06-17 18:12:41 +0700

    bb

--- RestaurantFoodReservationApplication/app/src/main/java/com/example/restaurantfoodreservationapplication/QuanLyMonActivity.java
+++ RestaurantFoodReservationApplication/app/src/main/java/com/example/restaurantfoodreservationapplication/QuanLyMonActivity.java
@@ -24,8 +24,12 @@ import android.widget.Toast;
 import com.example.restaurantfoodreservationapplication.Class.Mon_An;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 
 import java.io.InputStream;
@@ -35,14 +39,14 @@ import java.util.UUID;
 
 public class QuanLyMonActivity extends AppCompatActivity {
     Toolbar toolbar;
-    TextView add_btn;
+    TextView add_btn, update_btn, delete_btn;
     String dish_url;
     Bitmap bitmap;
     HashMap<String,String> hashMap = new HashMap<>();
     FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
  //   final String userID=firebaseUser.getUid();
     EditText code_res, name_res, price_res, purl_res;
-    boolean flagadd = false;
+    boolean flagadd = false, flagupdate = false, flagdelete = false;
     DatabaseReference monan_id;
 
     @Override
@@ -50,9 +54,10 @@ public class QuanLyMonActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.fragment_quan_ly_mon);
         setContentView(R.layout.activity_quan_ly_mon);
-
         toolbar = findViewById(R.id.toolbar);
         add_btn = findViewById(R.id.add_btn);
+        update_btn = findViewById(R.id.update_btn);
+        delete_btn = findViewById(R.id.delete_btn);
         code_res = findViewById(R.id.code_res);
         name_res = findViewById(R.id.name_res);
         price_res = findViewById(R.id.price_res);
@@ -61,6 +66,16 @@ public class QuanLyMonActivity extends AppCompatActivity {
         add_btn.setOnClickListener(v -> {
             addtofirebase();
         });
+
+        delete_btn.setOnClickListener(v -> {
+            deletefromfirebase();
+            Toast.makeText(this, "OK", Toast.LENGTH_SHORT).show();
+        });
+
+        update_btn.setOnClickListener(v -> {
+            updatetofirebase();
+        });
+
         //show back arrow
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -94,6 +109,28 @@ public class QuanLyMonActivity extends AppCompatActivity {
         }
     }
 
+    private void updatetofirebase(){
+
+    }
+
+    private void deletefromfirebase(){
+        DatabaseReference dbr = FirebaseDatabase.getInstance().getReference().child("MonAn");
+        Query query = dbr.orderByChild("tenMon").equalTo(name_res.toString()); //Sua lai ban cho phu hop
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot singleSnapshot : snapshot.getChildren()){
+                    Mon_An mon_an = singleSnapshot.getValue(Mon_An.class);
+                    dbr.child(singleSnapshot.getKey()).removeValue();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
     private void StoreLink(String url){
         ProgressDialog dialog = new ProgressDialog(this);
         if(flagadd == true){
